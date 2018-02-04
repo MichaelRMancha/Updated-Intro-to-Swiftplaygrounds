75d29dab0838cae0b09c5209a032b51474c1f440
Michael Mancha <mancham@hawaii.edu>
2018-02-03 15:24:46 -1000

    Finished uploading swiftplaygrounds
    Made a new swiftplaygounds to upload to the Githib

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var myFirstVariable: String
+myFirstVariable = "I've created my first variable"
+
+//we're going to do something with strings
+var mySecondVariable = "and look I added two strings together"
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+print(myThirdVariable)
+
+//assignment #2 Intro to true and false
+var appstate = false
+
+print(appstate)
+
+//comparing variables using > or <
+var numberOne = 5
+var numberTwo = 7
+
+//create a variable that holds the comparisson of the two variables we just created
+var compare = numberOne > numberTwo
+
